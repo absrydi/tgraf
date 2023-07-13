47f98b0f382c6bf278cfd9e755d237d351ada007
absrydi <139432368+absrydi@users.noreply.github.com>
2023-07-13 15:17:01 +0700

    Update tgraf011-install-termux.sh

--- tgraf011-install-termux.sh
+++ tgraf011-install-termux.sh
@@ -3,7 +3,7 @@ pkg install unzip
 pkg install lua
 unzip tgraf0.1.1-bin.zip
 mv binary tgraf0.0.1-bin
-mv tgraf0.1.1-bin /data/data/com.termux/files/usr/lib/
-cd /data/data/com.termux/files/usr/lib/tgraf0.1.1-bin
+mv tgraf0.1.1-bin ../../usr/lib/
+cd ../../usr/lib/tgraf0.1.1-bin
 mv tgraf011 ../../bin/
 $SHELL
