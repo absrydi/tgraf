e63aeace753bd439dbaf61bb385ec49f1a741219
absrydi <139432368+absrydi@users.noreply.github.com>
2023-07-15 16:57:39 +0700

    Delete tgraf011-install-termux.sh

--- tgraf011-install-termux.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-pkg install unzip
-pkg install lua
-unzip tgraf0.1.1-bin.zip
-mv binary tgraf0.1.1-bin
-mv tgraf0.1.1-bin ../../usr/lib/
-cd ../../usr/lib/tgraf0.1.1-bin
-mv tgraf011 ../../bin/
-$SHELL
